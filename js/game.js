5a641132b1cfa1d67c288027af14450234edd6e8
Lucas-Tyman <Lucas-Tyman@users.noreply.github.com>
2022-04-13 17:21:26 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -4,11 +4,11 @@
 // This file contains the JS functions for index.html
 
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
 
-backgroundColor: 0x5f6e7a
+  backgroundColor: 0x5f6e7a,
 }
 const game = new Phaser.Game(config)
 console.log(game)
