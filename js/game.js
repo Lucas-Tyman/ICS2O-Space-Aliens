1c93b3acebe5258eedf1ac78e6405671eaeecd50
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-22 17:22:59 +0000

    game.js

--- js/game.js
+++ js/game.js
@@ -8,10 +8,19 @@ const config = {
   width: 1920,
   height: 1080,
   physics: {
-    default: 'arcade"'
-  }
-
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background colour
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
+
 const game = new Phaser.Game(config)
 console.log(game)
