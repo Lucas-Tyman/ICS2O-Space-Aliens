7ab348ff1cc695fb025038f522d3dba0f25b68ea
Lucas-Tyman <Lucas-Tyman@users.noreply.github.com>
2022-04-26 18:00:43 +0000

    Prettified Code!

--- js/splashScene.js
+++ js/splashScene.js
@@ -44,7 +44,7 @@ class SplashScene extends Phaser.Scene {
     this.splashSceneBackgroundImage = this.add.sprite(0, 0)
     this.splashSceneBackgroundImage.x = 1920 / 2
     this.splashSceneBackgroundImage.y = 1080 / 2
-    "splashSceneBackGround"
+    ;("splashSceneBackGround")
   }
 
   /**
