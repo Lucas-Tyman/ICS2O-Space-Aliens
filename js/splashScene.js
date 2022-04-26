7e83df10af2fa3aac94f4951606b8e4fd1882e2a
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-26 13:50:11 +0000

    JSdoc

--- js/splashScene.js
+++ js/splashScene.js
@@ -3,21 +3,46 @@
 // Created on: Apr 2022
 // This is the Splash Scene
 
+/**
+ * This class is the Splash Scene.
+ */
 class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the constructuor.
+   */
   constructor() {
     super({ key: "splashScene" })
   }
 
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when scene starts,
+   *   before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
   init(data) {
     this.cameras.main.setBackgroundColor("#ffffff")
   }
 
+  /**
+   * Can be defined on your own Scenes
+   * Use it to load assets.
+   */
   preload() {
     console.log("Splash Scene")
   }
+  
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
+    // pass
+  }
 
-  create(data) {}
-
-  update(time, delta) {}
+  update(time, delta) {
+    // pass
+  }
 }
 export default SplashScene
